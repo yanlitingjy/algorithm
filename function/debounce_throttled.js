21393cbb7f8c9992f3cf44942421b7dbfcd09ec7
yanliting <yanliting@chinalawinfo.com>
2022-08-02 17:11:44 +0800

    修改防抖节流

--- function/debounce_throttled.js
+++ function/debounce_throttled.js
@@ -1,38 +1,33 @@
 /**
  * 防抖: n 秒后在执行该事件，若在 n 秒内被重复触发，则重新计时
  */
- function debounce(func, wait) {
-    let timeout;
-
-    return function () {
-        let context = this; // 保存this指向
-        let args = arguments; // 拿到event对象
-
-        clearTimeout(timeout)
-        timeout = setTimeout(function(){
-            func.apply(context, args)
-        }, wait);
+function debounce(fn,delay) {
+    // 定义一个定时器,保存上一次的定时器
+    let timer = null
+    return function(...args) {
+        // 取消上一次的定时器
+        if(timer) clearTimeout(timer)
+        // 延迟执行
+        timer = setTimeout(()=>{
+            fn.apply(this,args)
+        },delay)
     }
 }
 // 如果需要立即执行的话
-function debounce(func, wait, immediate) {
-    let timeout;
-    return function () {
-        let context = this;
-        let args = arguments;
-        if (timeout) clearTimeout(timeout); // timeout 不为null
-        if (immediate) {
-            let callNow = !timeout; // 第一次会立即执行，以后只有事件执行后才会再次触发
-            timeout = setTimeout(function () {
-                timeout = null;
-            }, wait)
-            if (callNow) {
-                func.apply(context, args)
-            }
-        }else {
-            timeout = setTimeout(function () {
-                func.apply(context, args)
-            }, wait);
+function debounce1(fn,delay,isImmediate) {
+    let timer = null
+    let isInvoke = false
+    return function(...args) {
+        if(timer) clearTimeout(timer)
+        // 判断是否需要立即执行
+        if(isImmediate && !isInvoke){
+            fn.apply(this,args)
+            isInvoke = true
+        }else{ // 延迟执行
+            timer = setTimeout(()=>{
+                fn.apply(this,args)
+                isInvoke = false
+            },delay)
         }
     }
 }
@@ -46,6 +41,26 @@ function throttled(fn, delay = 500) {
             timer = setTimeout(() => {
                 fn.apply(this, args)
                 timer = null
+            }, delay);
+        }
+    }
+}
+
+// 通过参数控制是否立即执行
+const throttle2 = (fn, delay, isImmediate) => {
+    let timer = null
+    let isInvoke = false
+    return function(...args) {
+        if (timer) return;
+        if (isImmediate && !isInvoke ) {
+            fn.apply(this, args);
+            timer = setTimeout(() => {
+                isInvoke = false;
+            }, delay);
+        } else {
+            timer = setTimeout(() => {
+                fn.apply(this, args);
+                isInvoke = false;
             }, delay);
         }
     }
